c7cab7930d7186723b556a2d03d4da7f8f45d2b8
sAyusha <ayushastha69@gmail.com>
2023-08-06 12:01:33 +0545

    unit testing

--- test_data/book_entity_test.dart
+++ test_data/book_entity_test.dart
@@ -14,3 +14,31 @@ Future<List<BookEntity>> getBookListTest() async {
 
   return Future.value(bookList);
 }
+
+Future<List<BookEntity>> getBookmarkedBooks() async {
+  final response =
+      await rootBundle.loadString('test_data/bookmarked_books_data.json');
+  final jsonList = await json.decode(response);
+
+  final List<BookEntity> bookmarked = jsonList
+      .map<BookEntity>(
+        (json) => BookEntity.fromJson(json),
+      )
+      .toList();
+
+  return Future.value(bookmarked);
+}
+
+Future<List<BookEntity>> getUserBooks() async {
+  final response =
+      await rootBundle.loadString('test_data/user_books_test.json');
+  final jsonList = await json.decode(response);
+
+  final List<BookEntity> userBooks = jsonList
+      .map<BookEntity>(
+        (json) => BookEntity.fromJson(json),
+      )
+      .toList();
+
+  return Future.value(userBooks);
+}
